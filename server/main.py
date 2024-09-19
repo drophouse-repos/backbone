617167e02ba190305ce77b857254111f43f0514e
Kush Bhuwalka <kushbhuwalka@gmail.com>
2024-09-18 21:42:33 -0500

    allow origins updated

--- server/main.py
+++ server/main.py
@@ -47,7 +47,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://superman-kappa.vercel.app",
-        "https://staging-fe-v2.vercel.app",
+        "https://admin.drophouse.ai",
         "https://drophouse.rose-hulman.edu",
     ],
     allow_origin_regex=r".*\.drophouse\.ai$",
