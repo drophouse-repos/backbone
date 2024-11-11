e5ae65aa8839bb7a23501ac0b8fba8281d51ec0a
batman-drophouse <batman@drophouse.ai>
2024-11-11 08:30:29 +0530

    ornament change

--- server/routers/stripe.py
+++ server/routers/stripe.py
@@ -176,7 +176,7 @@ async def create_checkout_session(
             "price_data": {
                 "currency": "usd",
                 "product_data": {
-                    "name": f"Rose Hulman {color} {apparel}",
+                    "name": f"{color} {apparel}" if (apparel != 'mug' and apparel != 'Mug') else f"{org_name} mug/ornament",
                     "description": f"{item.prompt}\nSize: {item.size}",
                     "images": [thumbnail],
                     # "metadata": {
